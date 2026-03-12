d943ebabc9fde534581d2d69492fa127233d8604
Sergiu Romaniuc <sergiu.romaniuc01@gmail.com>
2026-03-12 23:48:15 +0200

    changes for the auth; changes for the loading and saving plans

--- frontend/js/storage.js
+++ frontend/js/storage.js
@@ -1,9 +1,11 @@
 // localStorage logic
 
 export function loadPlans(){
+    console.log('Loaded plans:' + localStorage.getItem('plans') || {})
     return JSON.parse(localStorage.getItem('plans')) || {};
 }
 
 export function savePlans(plans){
+    console.log('Saving plans:' + plans)
     localStorage.setItem('plans', JSON.stringify(plans));
 }
